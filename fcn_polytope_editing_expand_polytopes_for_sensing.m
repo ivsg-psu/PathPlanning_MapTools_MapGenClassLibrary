81e7483489f02dcddd9703d9d3a8bbd972c4f6e7
STau01 <67921757+STau01@users.noreply.github.com>
2021-04-28 12:40:17 -0400

    added header to sensing dilation function

--- fcn_polytope_editing_expand_polytopes_for_sensing.m
+++ fcn_polytope_editing_expand_polytopes_for_sensing.m
@@ -1,26 +1,53 @@
 function [sensing_polytopes] = fcn_polytope_editing_expand_polytopes_for_sensing(polytopes,delta,exp_dist)
-% expands polytopes while cutting off corners where radii would be to get
-% a conservative estimate of when a polytope would be sensed
-
-% %% size sensing polytopes
-% sensing_polytopes = polytopes;
-% 
-% %% expand poltyopes evenly as normal
-% exp_polytopes = fcn_polytope_editing_expand_polytopes_evenly(polytopes,delta,exp_dist);
-% 
-% %% modify polytopes to add psudo radii
-% for polys = 1:size(exp_polytopes,2)
-%     % pull values
-%     xv = exp_polytopes(polys).xv;
-%     yv = exp_polytopes(polys).yv;
-%     
-%     % reorginize for easier use
-%     x = ([xv(end) xv xv(1)]-centroid(1))*scale; % x values to modify
-%     y = [yv(end) yv yv(1)]; % y values to modify
-%     
-%     
-%     % modify corners
-%     for vert = 1:length(xv)
+% FCN_POLYTOPE_EDITING_EXPAND_POLYTOPES_FOR_SENSING expands polytopes while
+% cutting off corners where radii would be to get a conservative estimate 
+% of when a polytope would be sensed
+%
+% [SENSING_POLYTOPES]=FCN_POLYTOPE_EDITING_EXPAND_POLYTOPES_EVENLY(POLYTOPES,DELTA,EXP_DIST)
+% returns:
+% SENSING_POLYTOPES: a 1-by-n seven field structure of sensing polytopes, 
+%   where n <= NUM_POLY, with fields:
+% vertices: a m+1-by-2 matrix of xy points with row1 = rowm+1, where m is
+%   the number of the individual polytope vertices
+% xv: a 1-by-m vector of vertice x-coordinates
+% yv: a 1-by-m vector of vertice y-coordinates
+% distances: a 1-by-m vector of perimeter distances from one point to the
+%   next point, distances(i) = distance from vertices(i) to vertices(i+1)
+% mean: average xy coordinate of the polytope
+% area: area of the polytope
+% max_radius: distance of the farthest vertex from the mean
+%
+% with inputs:
+% POLYTOPES: the original 1-by-n seven field structure with the same fields
+% DELTA: a small number relative to vehicle size to determine the inside of
+%   an obstacle
+% EXP_DIST: distance to expand the obstacle
+%
+% Examples:
+%      
+%      xv = [-2 -1 1 2 2 1 -1 -2];
+%      yv = [-1 -2 -2 -1 1 2 2 1];
+%      polytopes.vertices = [[xv xv(1)]' [yv yv(1)]'];
+%      polytopes.xv = xv;
+%      polytopes.yv = yv;
+%      polytopes.distances = fcn_general_calculation_euclidean_point_to_point_distance(polytopes(1).vertices(1:end-1,:),polytopes(1).vertices(2:end,:));
+%      [Cx,Cy,polytopes.area] = fcn_polytope_calculation_centroid_and_area([xv xv(1)],[yv yv(1)]);
+%      polytopes.mean = [Cx, Cy];
+%      polytopes.max_radius = max(fcn_general_calculation_euclidean_point_to_point_distance(polytopes.vertices(1:end-1,:),ones(length(xv),1)*polytopes.mean));
+%      delta = 0.01;
+%      exp_dist = 1;
+%      sensing_polytopes=fcn_polytope_editing_expand_polytopes_for_sensing(polytopes,delta,exp_dist);
+%      fcn_plot_polytopes(polytopes,99,'r-',2);
+%      fcn_plot_polytopes(sensing_polytopes,99,'b-',2,[-4 4 -4 4],'square');
+%      legend('Original','Expanded')
+%      box on
+%      xlabel('X Position')
+%      ylabel('Y Position')
+%
+% This function was written on 2020_04_17 by Seth Tau
+% Adjusted example code on 2021_04_28 by Seth Tau
+% Questions or comments? sat5340@psu.edu 
+%
         
         
 %% Check input arguments
