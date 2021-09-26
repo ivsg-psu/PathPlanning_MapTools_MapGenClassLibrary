d340185e3a017acafaf6713bce73b9de5fbdb2fd
sjharnett <sjh6473@psu.edu>
2021-09-26 13:15:00 -0400

    more robust file extension check

--- Functions/script_test_fcn_MapGen_all.m
+++ Functions/script_test_fcn_MapGen_all.m
@@ -2,12 +2,12 @@
 % class library for the purpose of evaluating every assertion test in these
 % files
 clear all; close all; clc;
-all_scripts = dir('script_test_fcn_*');
+all_scripts = dir('script_test_fcn_*.m');
 diary 'script_test_fcn_MapGen_all_stdout.txt';
 for i_script = 1:length(all_scripts)
     file_name_extended = all_scripts(i_script).name;
     file_name = erase(file_name_extended,'.m');
-    if ~strcmp(mfilename,file_name) && ~strcmp(file_name(end-3:end),'.asv')
+    if ~strcmp(mfilename,file_name)
         file_name_trunc = erase(file_name,'script_');
         suite = testsuite(file_name);
         results = run(suite);
