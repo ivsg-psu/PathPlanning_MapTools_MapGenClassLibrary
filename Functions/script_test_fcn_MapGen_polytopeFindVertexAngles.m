c615fc610cc0fb4c3063728543bbd32753d1b208
sjharnett <sjh6473@psu.edu>
2021-10-25 21:46:28 -0400

    apply angle processing to entire obstacle field of polytopes

--- Functions/script_test_fcn_MapGen_polytopeFindVertexAngles.m
+++ Functions/script_test_fcn_MapGen_polytopeFindVertexAngles.m
@@ -57,45 +57,62 @@ function main()
 
     % Basic example of vertex calculation
     fig_num = 11;
-    [angles, unit_in_vectors, unit_out_vectors] =...
-        fcn_MapGen_polytopeFindVertexAngles(...
-        shrinker.vertices,fig_num);
-    assert(1000*eps>abs(360-sum(angles)*180/pi));
-    vertices = shrinker.vertices;
-    mean_vectors = (unit_out_vectors-unit_in_vectors)/2;
-    length_mean_vectors = sum(mean_vectors.^2,2).^0.5;
-    unit_direction_of_cut = mean_vectors./length_mean_vectors;
-    quiver(vertices(1:end-1,1),vertices(1:end-1,2),unit_direction_of_cut(:,1),unit_direction_of_cut(:,2),'g')
-    vertex_normal_vectors = [unit_direction_of_cut(:,1),unit_direction_of_cut(:,2)]
-    % vertex_normal_vectors = circshift(vertex_normal_vectors,1)
-    side_vectors = zeros(length(vertices)-1,2);
-    theta_normal_to_side = zeros(length(vertices)-1,1);
-    angle_vertex_normal_to_travel_direction = zeros(length(vertices)-1,1);
-    is_away = zeros(1, length(vertices)-1);
-    travel_direction = [1,0];
-    away_normals = NaN(length(vertices)-1,2);
-    away_angles = NaN(length(vertices)-1,1);
-    away_angle_vertex_normal_to_travel_direction = NaN(length(vertices)-1,1);
-    for i=1:length(vertices)-1
-        is_away(i) = dot(vertex_normal_vectors(i,:),travel_direction);
-        angle_vertex_normal_to_travel_direction(i) = angle_between_vectors(vertex_normal_vectors(i,:),travel_direction);
-        if is_away(i)>0
-            away_normals(i,:) = vertex_normal_vectors(i,:);
-            away_angles(i) = angles(i);
-            away_angle_vertex_normal_to_travel_direction(i) =  angle_vertex_normal_to_travel_direction(i);
+    fig_num = 12;
+    Halton_range = [1 1000]; % range of Halton points to use to generate the tiling
+    tiled_polytopes = fcn_MapGen_haltonVoronoiTiling(Halton_range,[1 1],fig_num);
+    title('Halton set');
+    field_away_normals = [];
+    field_away_angles = [];
+    field_away_angle_vertex_normal_to_travel_direction = [];
+    field_small_choice_angles = [];
+    field_big_choice_angles = [];
+    for i=1:length(tiled_polytopes)
+        shrinker = tiled_polytopes(i);
+        [angles, unit_in_vectors, unit_out_vectors] =...
+            fcn_MapGen_polytopeFindVertexAngles(...
+            shrinker.vertices,fig_num);
+        assert(1000*eps>abs(360-sum(angles)*180/pi));
+        vertices = shrinker.vertices;
+        mean_vectors = (unit_out_vectors-unit_in_vectors)/2;
+        length_mean_vectors = sum(mean_vectors.^2,2).^0.5;
+        unit_direction_of_cut = mean_vectors./length_mean_vectors;
+        quiver(vertices(1:end-1,1),vertices(1:end-1,2),unit_direction_of_cut(:,1),unit_direction_of_cut(:,2),'g')
+        vertex_normal_vectors = [unit_direction_of_cut(:,1),unit_direction_of_cut(:,2)]
+        % vertex_normal_vectors = circshift(vertex_normal_vectors,1)
+        side_vectors = zeros(length(vertices)-1,2);
+        theta_normal_to_side = zeros(length(vertices)-1,1);
+        angle_vertex_normal_to_travel_direction = zeros(length(vertices)-1,1);
+        is_away = zeros(1, length(vertices)-1);
+        travel_direction = [1,0];
+        away_normals = NaN(length(vertices)-1,2);
+        away_angles = NaN(length(vertices)-1,1);
+        away_angle_vertex_normal_to_travel_direction = NaN(length(vertices)-1,1);
+        for i=1:length(vertices)-1
+            is_away(i) = dot(vertex_normal_vectors(i,:),travel_direction);
+            angle_vertex_normal_to_travel_direction(i) = angle_between_vectors(vertex_normal_vectors(i,:),travel_direction);
+            if is_away(i)>0
+                away_normals(i,:) = vertex_normal_vectors(i,:);
+                away_angles(i) = angles(i);
+                away_angle_vertex_normal_to_travel_direction(i) =  angle_vertex_normal_to_travel_direction(i);
+            end
         end
-    end
-    away_normals(any(isnan(away_angles),2),:)=[];
-    away_angles(any(isnan(away_angles),2),:)=[];
-    away_angle_vertex_normal_to_travel_direction(any(isnan(away_angle_vertex_normal_to_travel_direction),2),:)=[];
-    small_choice_angles = away_angles./2-away_angle_vertex_normal_to_travel_direction;
-    big_choice_angles = away_angles./2+away_angle_vertex_normal_to_travel_direction;
-    for i=1:length(vertices)-1
-        side_vectors(i,1) = vertices(i+1,1)-vertices(i,1);
-        side_vectors(i,2) = vertices(i+1,2)-vertices(i,2);
-        quiver(vertices(i,1),vertices(i,2),vertices(i+1,1)-vertices(i,1),vertices(i+1,2)-vertices(i,2),'-b');
-        % theta = arccos((x dot y)/(mag x * mag y))
-        theta_normal_to_side(i) = angle_between_vectors(side_vectors(i,:),vertex_normal_vectors(i,:));
+        away_normals(any(isnan(away_angles),2),:)=[];
+        away_angles(any(isnan(away_angles),2),:)=[];
+        away_angle_vertex_normal_to_travel_direction(any(isnan(away_angle_vertex_normal_to_travel_direction),2),:)=[];
+        small_choice_angles = away_angles./2-away_angle_vertex_normal_to_travel_direction;
+        big_choice_angles = away_angles./2+away_angle_vertex_normal_to_travel_direction;
+        field_away_normals = [field_away_normals;away_normals];
+        field_away_angles = [field_away_angles;away_angles];
+        field_away_angle_vertex_normal_to_travel_direction = [field_away_angle_vertex_normal_to_travel_direction;away_angle_vertex_normal_to_travel_direction];
+        field_small_choice_angles = [field_small_choice_angles;small_choice_angles];
+        field_big_choice_angles = [field_big_choice_angles;big_choice_angles];
+        for i=1:length(vertices)-1
+            side_vectors(i,1) = vertices(i+1,1)-vertices(i,1);
+            side_vectors(i,2) = vertices(i+1,2)-vertices(i,2);
+            quiver(vertices(i,1),vertices(i,2),vertices(i+1,1)-vertices(i,1),vertices(i+1,2)-vertices(i,2),'-b');
+            % theta = arccos((x dot y)/(mag x * mag y))
+            theta_normal_to_side(i) = angle_between_vectors(side_vectors(i,:),vertex_normal_vectors(i,:));
+        end
     end
 end
 function ang = angle_between_vectors(a,b)
