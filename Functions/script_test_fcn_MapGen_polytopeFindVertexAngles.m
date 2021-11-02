8db317ec645a0419dbd2d08426073ca7eca39774
sjharnett <sjh6473@psu.edu>
2021-11-02 12:56:35 -0400

    functional plotting

--- Functions/script_test_fcn_MapGen_polytopeFindVertexAngles.m
+++ Functions/script_test_fcn_MapGen_polytopeFindVertexAngles.m
@@ -14,7 +14,7 @@ function main()
     r_lc_avg_all = [];
     r_lc_iterative_all = [];
     % shrink_distance = [];
-    for tiles=50:10:70%10:80:500
+    for tiles=50:25:75%10:80:500
         Halton_range = [1 tiles]; % range of Halton points to use to generate the tiling
         tiled_polytopes = fcn_MapGen_haltonVoronoiTiling(Halton_range,[1 1],fig_num);
         title('Halton set');
@@ -28,30 +28,35 @@ function main()
         r_lc_avg_all = [r_lc_avg_all, r_lc_avg];
         r_lc_iterative_all = [r_lc_iterative_all, r_lc_iterative];
         % shrink_distance = [shrink_distance, 0];
-%         for radii_goals=0.01:0.01:0.1%0.001:0.010:0.1
-%             try
-%                 des_rad = radii_goals; sigma_radius = 0; min_rad = 0.001;
-%                 [shrunk_field,mu_final,sigma_final] = fcn_MapGen_polytopesShrinkToRadius(tiled_polytopes,des_rad,sigma_radius,min_rad,fig_num);
-%                 field_stats = fcn_MapGen_polytopesStatistics(shrunk_field);
-%                 field_avg_r_D = field_stats.avg_r_D;
-%                 r_D = [r_D, field_avg_r_D];
-%                 % avg_max_rad = field_stats.average_max_radius;
-%                 % shrink_distance = [shrink_distance, avg_max_rad-des_rad];
-%                 [r_lc_max,r_lc_avg,r_lc_iterative] = do_r_LC(shrunk_field)
-%                 r_lc_max_all = [r_lc_max_all, r_lc_max];
-%                 r_lc_avg_all = [r_lc_avg_all, r_lc_avg];
-%                 r_lc_iterative_all = [r_lc_iterative_all, r_lc_iterative];
-%             end
-%         end
+        for radii_goals=0.02:0.02:0.1%0.001:0.010:0.1
+            try
+                des_rad = radii_goals; sigma_radius = 0; min_rad = 0.001;
+                [shrunk_field,mu_final,sigma_final] = fcn_MapGen_polytopesShrinkToRadius(tiled_polytopes,des_rad,sigma_radius,min_rad,fig_num);
+                field_stats = fcn_MapGen_polytopesStatistics(shrunk_field);
+                field_avg_r_D = field_stats.avg_r_D;
+                r_D = [r_D, field_avg_r_D];
+                % avg_max_rad = field_stats.average_max_radius;
+                % shrink_distance = [shrink_distance, avg_max_rad-des_rad];
+                [r_lc_max,r_lc_avg,r_lc_iterative] = do_r_LC(shrunk_field)
+                r_lc_max_all = [r_lc_max_all, r_lc_max];
+                r_lc_avg_all = [r_lc_avg_all, r_lc_avg];
+                r_lc_iterative_all = [r_lc_iterative_all, r_lc_iterative];
+            end
+        end
     end
     close all
     figure
     plot(r_D,'ko')
+    title('r_D distribution')
     figure(609)
+    title('r_D versus r_{LC}')
+    xlabel('r_D from average radius')
+    ylabel('r_{LC} from side length and vertex angle')
     hold on
-    plot(r_lc_max_all,'ro')
-    plot(r_lc_avg_all,'bo')
-    plot(r_lc_iterative_all,'go')
+    plot(r_D,r_lc_max_all,'ro')
+    plot(r_D,r_lc_avg_all,'bo')
+    plot(r_D,r_lc_iterative_all,'go')
+    legend('theoretical max from half of average angle','average from average side length and average angle','measured from subset of polytopes')
 %     figure
 %     plot(shrink_distance,r_D,'go')
     % end r_D range generation
