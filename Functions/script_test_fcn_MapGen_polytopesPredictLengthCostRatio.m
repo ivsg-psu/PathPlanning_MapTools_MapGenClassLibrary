8c03a50628196855a03dd9eaf8da358e2264f3d1
sjharnett <sjh6473@psu.edu>
2022-01-19 20:29:00 -0500

    plot different standard deviations as different series

--- Functions/script_test_fcn_MapGen_polytopesPredictLengthCostRatio.m
+++ Functions/script_test_fcn_MapGen_polytopesPredictLengthCostRatio.m
@@ -5,7 +5,7 @@
 % 2021_10_22
 % -- first written by S. Harnett
 
-clear all; close all;clc;
+% clear all; close all;clc;
 fig_num = 1;
 
 do_single_test = false;
@@ -63,11 +63,13 @@ if do_range_test
         r_lc_sparse_std_all = [r_lc_sparse_std_all, r_lc_sparse_std];
         shrink_distance = [shrink_distance, 0];
         % for radii_goals=0.25%0.02:0.02:0.1%0.001:0.010:0.1
-        for radii_goals = 0.001:0.005:0.081
-            for sd_radius = 0:0.005:0.032
+        sd_radius_values = [0, 0.01, 0.02, 0.04, 0.08, 0.16, 0.32];
+        for sd_radius_index = 1:1:length(sd_radius_values)
+            sd_radius = sd_radius_values(sd_radius_index);
+            for radii_goals = 0.001:0.005:0.081
                 des_rad = radii_goals; sigma_radius = sd_radius; min_rad = 0.001;
                 % TODO switch this to side shrinking to get gap distance as an output so it can be given to predictor as input
-                [shrunk_field,mu_final,sigma_final] = fcn_MapGen_polytopesShrinkFromEdges(tiled_polytopes,des_rad,sigma_radius,min_rad);%,fig_num);
+                [shrunk_field,mu_final,sigma_final] = fcn_MapGen_polytopesShrinkToRadius(tiled_polytopes,des_rad,sigma_radius,min_rad);%,fig_num);
                 field_stats = fcn_MapGen_polytopesStatistics(shrunk_field);
                 gap_size = field_stats.average_gap_size_G_bar;
                 field_avg_r_D = field_stats.avg_r_D;
@@ -79,20 +81,39 @@ if do_range_test
                     tiles_failed = [tiles_failed, tiles];
                     size_percent_failed = [size_percent_failed, size_percent];
                 end
-                r_lc_max_all = [r_lc_max_all, r_lc_max];
-                r_lc_max_effective_all = [r_lc_max_effective_all, r_lc_max_effective];
-                r_lc_avg_all = [r_lc_avg_all, r_lc_avg];
-                r_lc_avg_effective_all = [r_lc_avg_effective_all, r_lc_avg_effective];
-                r_lc_iterative_all = [r_lc_iterative_all, r_lc_iterative];
-                r_lc_iterative_effective_all = [r_lc_iterative_effective_all, r_lc_iterative_effective];
+%                 r_lc_max_all = [r_lc_max_all, r_lc_max];
+%                 r_lc_max_effective_all = [r_lc_max_effective_all, r_lc_max_effective];
+%                 r_lc_avg_all = [r_lc_avg_all, r_lc_avg];
+%                 r_lc_avg_effective_all = [r_lc_avg_effective_all, r_lc_avg_effective];
+%                 r_lc_iterative_all = [r_lc_iterative_all, r_lc_iterative];
+%                 r_lc_iterative_effective_all = [r_lc_iterative_effective_all, r_lc_iterative_effective];
                 r_lc_sparse_worst_all = [r_lc_sparse_worst_all, r_lc_sparse_worst];
                 r_lc_sparse_average_all = [r_lc_sparse_average_all, r_lc_sparse_average];
                 r_lc_sparse_std_all = [r_lc_sparse_std_all, r_lc_sparse_std];
                 r_D = [r_D, field_avg_r_D];
-                close all;
-           end
+            end
+            figure(3)
+            hold on
+            try
+%                 plot(r_D,r_lc_max_all,'ro')
+%                 plot(r_D,r_lc_avg_all,'bo')
+%                 plot(r_D,r_lc_iterative_all,'go')
+%                 plot(r_D,r_lc_max_effective_all,'rx')
+%                 plot(r_D,r_lc_avg_effective_all,'bx')
+%                 plot(r_D,r_lc_iterative_effective_all,'gx')
+                plot(r_D,r_lc_sparse_worst_all,'md')
+                % plot(r_D,r_lc_sparse_average_all,'cd')
+                % positive and negative errorbars
+                % errorbar(r_D,r_lc_sparse_average_all,2*r_lc_sparse_std_all,'cd')
+                % positive only errorbars
+                errorbar(r_D,r_lc_sparse_average_all,zeros(1,length(r_lc_sparse_average_all)),2*r_lc_sparse_std_all,zeros(1,length(r_lc_sparse_average_all)),zeros(1,length(r_lc_sparse_average_all)),'cd')
+            end
+            r_lc_sparse_worst_all = [];
+            r_lc_sparse_average_all = [];
+            r_lc_sparse_std_all = [];
+            r_D = [];
         end
-    end%     close all
+    end
    fprintf('Obstacle fields that could not be predicted:\n')
    fprintf('Num. tiles | Size percent\n')
    fprintf('%d        | %d\n',tiles_failed,size_percent_failed)
@@ -107,45 +128,32 @@ if plot_flag
     xlabel('r_D from average radius')
     ylabel('r_{LC} from side length and vertex angle')
     hold on
-    try
-        plot(r_D,r_lc_max_all,'ro')
-        plot(r_D,r_lc_avg_all,'bo')
-        plot(r_D,r_lc_iterative_all,'go')
-        plot(r_D,r_lc_max_effective_all,'rx')
-        plot(r_D,r_lc_avg_effective_all,'bx')
-        plot(r_D,r_lc_iterative_effective_all,'gx')
-        plot(r_D,r_lc_sparse_worst_all,'md')
-        % plot(r_D,r_lc_sparse_average_all,'cd')
-        % errorbar(r_D,r_lc_sparse_average_all,2*r_lc_sparse_std_all,'cd')
-        % positive only errorbars
-        errorbar(r_D,r_lc_sparse_average_all,zeros(1,length(r_lc_sparse_average_all)),2*r_lc_sparse_std_all,zeros(1,length(r_lc_sparse_average_all)),zeros(1,length(r_lc_sparse_average_all)),'cd')
-    end
-    x1 = linspace(0,0.65,300);
-    x2=linspace(0.65,.78,100);
-    k1 = 0.4124*x1+41.91*x1.^2;
-    k2 = 0.4124*0.65+41.91*0.65^2-120.3*(x2-0.65)+17.47*(x2-0.65).^2;
-    t1 = 0.0048*x1-0.0016*x1.^2;
-    t2 = 0.0048*0.68-0.0016*0.65^2-0.0009/((0.8118-0.65)^1.25)+0.0009./((0.8118-x2).^1.25);
-    mean1 = k1.*t1 + 1;
-    mode1 = (k1-1).*t1+1;
-    mean2 = k2.*t2 + 1;
-    mode2 = (k2-1).*t2+1;
-    var1 = k1.*t1.^2;
-    var2 = k2.*t2.^2;
-    sd1 = (var1).^2;
-    sd2 = (var2).^2;
-    plot(x1,mean1,'k-')
-    plot(x2,mean2,'k-')
-    bot1 = mean1 - var1;
-    bot2 = mean2 - var2;
-    top1 = mean1 + var1;
-    top2 = mean2 + var2;
-    plot(x1,bot1,'g-');
-    plot(x2,bot2,'g-');
-    plot(x1,top1,'g-');
-    plot(x2,top2,'g-');
-    patch([x1 fliplr(x1)], [bot1 fliplr(top1)], 'g','FaceAlpha',0.2);
-    patch([x2 fliplr(x2)], [bot2 fliplr(top2)], 'g','FaceAlpha',0.2);
+%     x1 = linspace(0,0.65,300);
+%     x2=linspace(0.65,.78,100);
+%     k1 = 0.4124*x1+41.91*x1.^2;
+%     k2 = 0.4124*0.65+41.91*0.65^2-120.3*(x2-0.65)+17.47*(x2-0.65).^2;
+%     t1 = 0.0048*x1-0.0016*x1.^2;
+%     t2 = 0.0048*0.68-0.0016*0.65^2-0.0009/((0.8118-0.65)^1.25)+0.0009./((0.8118-x2).^1.25);
+%     mean1 = k1.*t1 + 1;
+%     mode1 = (k1-1).*t1+1;
+%     mean2 = k2.*t2 + 1;
+%     mode2 = (k2-1).*t2+1;
+%     var1 = k1.*t1.^2;
+%     var2 = k2.*t2.^2;
+%     sd1 = (var1).^2;
+%     sd2 = (var2).^2;
+%     plot(x1,mean1,'k-')
+%     plot(x2,mean2,'k-')
+%     bot1 = mean1 - var1;
+%     bot2 = mean2 - var2;
+%     top1 = mean1 + var1;
+%     top2 = mean2 + var2;
+%     plot(x1,bot1,'g-');
+%     plot(x2,bot2,'g-');
+%     plot(x1,top1,'g-');
+%     plot(x2,top2,'g-');
+%     patch([x1 fliplr(x1)], [bot1 fliplr(top1)], 'g','FaceAlpha',0.2);
+%     patch([x2 fliplr(x2)], [bot2 fliplr(top2)], 'g','FaceAlpha',0.2);
     legend('theoretical max',...
         'average from side and angle',...
         'iterative from side and angle',...
@@ -156,5 +164,4 @@ if plot_flag
         'sparse formula, average case',...
         'gamma distribution curve fit',...
         'gamma distribution +/- one SD');
-    figure
 end
