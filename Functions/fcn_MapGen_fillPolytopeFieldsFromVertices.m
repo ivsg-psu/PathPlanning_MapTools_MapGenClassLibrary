b65719305ae8b7991eeb5a137f5f38e7a56efc7f
Stephen J. Harnett <sjh6473@arl.psu.edu>
2022-05-19 11:14:13 -0400

    adding cost and min radius fields to generated polys

--- Functions/fcn_MapGen_fillPolytopeFieldsFromVertices.m
+++ Functions/fcn_MapGen_fillPolytopeFieldsFromVertices.m
@@ -6,13 +6,13 @@ polytopes, ...
 varargin...
 )
 % fcn_MapGen_fillPolytopeFieldsFromVertices
-% Given a polytoope structure array where the vertices field is filled, 
+% Given a polytoope structure array where the vertices field is filled,
 % calculates the values for all the other fields.
-% 
-% 
-% 
+%
+%
+%
 % FORMAT:
-% 
+%
 %    [ ...
 %    filled_polytopes ...
 %    ] = ...
@@ -20,58 +20,58 @@ varargin...
 %    polytopes, ...
 %    (fig_num) ...
 %    )
-% 
+%
 % INPUTS:
-% 
-%     polytopes: an individual structure or structure array of 'polytopes' 
+%
+%     polytopes: an individual structure or structure array of 'polytopes'
 %     type that stores the polytopes to be filled
-% 
+%
 %     (optional inputs)
 %
-%     fig_num: any number that acts as a figure number output, causing a 
+%     fig_num: any number that acts as a figure number output, causing a
 %     figure to be drawn showing results.
-% 
-% 
+%
+%
 % OUTPUTS:
-% 
+%
 %     filled_polytopes: the polytopes array with all fields completed
-% 
-% 
+%
+%
 % DEPENDENCIES:
-% 
+%
 %     fcn_MapGen_polytopeCentroidAndArea
 %     fcn_MapGen_checkInputsToFunctions
-% 
-% 
+%
+%
 % EXAMPLES:
-% 
+%
 % See the script: script_test_fcn_MapGen_fillPolytopeFieldsFromVertices
 % for a full test suite.
-% 
+%
 % This function was written on 2021_07_02 by Sean Brennan
 % Questions or comments? contact sbrennan@psu.edu
 
-% 
+%
 % REVISION HISTORY:
-% 
+%
 % 2021_07_02 by Sean Brennan
 % -- first write of function
 
-% 
+%
 % TO DO:
-% 
+%
 % -- fill in to-do items here.
 
 %% Debugging and Input checks
-flag_check_inputs = 1; % Set equal to 1 to check the input arguments 
-flag_do_plot = 0;      % Set equal to 1 for plotting 
-flag_do_debug = 0;     % Set equal to 1 for debugging 
+flag_check_inputs = 1; % Set equal to 1 to check the input arguments
+flag_do_plot = 0;      % Set equal to 1 for plotting
+flag_do_debug = 0;     % Set equal to 1 for debugging
 
 if flag_do_debug
     fig_for_debug = 3;
     st = dbstack; %#ok<*UNRCH>
     fprintf(1,'STARTING function: %s, in file: %s\n',st(1).name,st(1).file);
-end 
+end
 
 %% check input arguments?
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -94,16 +94,16 @@ if 1 == flag_check_inputs
         error('Incorrect number of input arguments')
     end
 
-    % Check the polytopes input, make sure it has vertices   
+    % Check the polytopes input, make sure it has vertices
     if ~isfield(polytopes,'vertices')
         error('Field of vertices was not found');
     end
-    
+
     % Check the vertices input to have 4 or more rows, 2 columns
     %     fcn_MapGen_checkInputsToFunctions(...
     %         polytopes.vertices, '2column_of_numbers',[4 5]);
-    
- 
+
+
 end
 
 % Does user want to show the plots?
@@ -135,18 +135,18 @@ filled_polytopes = polytopes;
 num_poly = length(polytopes);
 
 for ith_poly = 1:num_poly % pull each polytope
-    
+
     % adjust polytopes
     filled_polytopes(ith_poly).xv        = (polytopes(ith_poly).vertices(1:end-1,1)');
     filled_polytopes(ith_poly).yv        = (polytopes(ith_poly).vertices(1:end-1,2)');
     filled_polytopes(ith_poly).distances = ...
         sum((polytopes(ith_poly).vertices(1:end-1,:) - ...
         polytopes(ith_poly).vertices(2:end,:)).^2,2).^0.5;
-    
+
     % Calculate the mean and area
     [filled_polytopes(ith_poly).mean,filled_polytopes(ith_poly).area] = ...
         fcn_MapGen_polytopeCentroidAndArea(polytopes(ith_poly).vertices);
-    
+
     % Find max radius
     radii = sum(...
         (filled_polytopes(ith_poly).vertices(1:end-1,:) - ...
@@ -155,6 +155,7 @@ for ith_poly = 1:num_poly % pull each polytope
         max(radii);
     filled_polytopes(ith_poly).min_radius = ...
         min(radii);
+    filled_polytopes(ith_poly).cost = rand;
 end
 
 %§
@@ -175,17 +176,17 @@ end
 if flag_do_plot
     figure(fig_num);
     hold on
-    
+
     % plot the polytopes
     fcn_MapGen_plotPolytopes(filled_polytopes,fig_num,'b',2);
-      
+
     % plot the means in black
     temp = zeros(length(filled_polytopes),2);
     for ith_poly = 1:length(filled_polytopes)
         temp(ith_poly,:) = filled_polytopes(ith_poly).mean;
     end
     plot(temp(:,1),temp(:,2),'ko','Markersize',3);
-end % Ends the flag_do_plot if statement    
+end % Ends the flag_do_plot if statement
 
 if flag_do_debug
     fprintf(1,'ENDING function: %s, in file: %s\n\n',st(1).name,st(1).file);
@@ -196,13 +197,13 @@ end % Ends the function
 
 %% Functions follow
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%   ______                _   _                 
-%  |  ____|              | | (_)                
-%  | |__ _   _ _ __   ___| |_ _  ___  _ __  ___ 
+%   ______                _   _
+%  |  ____|              | | (_)
+%  | |__ _   _ _ __   ___| |_ _  ___  _ __  ___
 %  |  __| | | | '_ \ / __| __| |/ _ \| '_ \/ __|
 %  | |  | |_| | | | | (__| |_| | (_) | | | \__ \
 %  |_|   \__,_|_| |_|\___|\__|_|\___/|_| |_|___/
-%                                               
+%
 % See: https://patorjk.com/software/taag/#p=display&f=Big&t=Functions
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%§
 
