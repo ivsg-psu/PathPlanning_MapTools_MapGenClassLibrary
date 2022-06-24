8570cdd27f4a75db78fac0a2a60d8afac085ef43
sjharnett <sjh6473@psu.edu>
2022-06-24 09:54:47 -0400

    adding comments, putting plotting code behind flag

--- Functions/fcn_MapGen_polytopesPredictLengthCostRatio.m
+++ Functions/fcn_MapGen_polytopesPredictLengthCostRatio.m
@@ -48,9 +48,9 @@ function [field_small_choice_angles,field_big_choice_angles,r_lc_max,r_lc_avg,r_
     % TODO add outputs for chosen angle, chosen side length etc
 
     %% Debugging and Input checks
-flag_check_inputs = 1; % Set equal to 1 to check the input arguments
-flag_do_plot = 0;      % Set equal to 1 for plotting
-flag_do_debug = 0;     % Set equal to 1 for debugging
+    flag_check_inputs = 1; % Set equal to 1 to check the input arguments
+    flag_do_plot = 0;      % Set equal to 1 for plotting
+    flag_do_debug = 0;     % Set equal to 1 for debugging
     if flag_do_debug
         fig_for_debug = 747;
         fig_num = 2100;
@@ -110,47 +110,55 @@ flag_do_debug = 0;     % Set equal to 1 for debugging
     % begin looping through polytopes in a field
     for j=1:length(tiled_polytopes)
         shrinker = tiled_polytopes(j);
+        vertices = shrinker.vertices;
         if flag_do_plot
             [angles, unit_in_vectors, unit_out_vectors] =...
             fcn_MapGen_polytopeFindVertexAngles(...
-            shrinker.vertices,fig_num);
+            vertices,fig_num);
         else
             [angles, unit_in_vectors, unit_out_vectors] =...
             fcn_MapGen_polytopeFindVertexAngles(...
-            shrinker.vertices);
+            vertices);
         end
-        assert(1000*eps>abs(360-sum(angles)*180/pi));
-        vertices = shrinker.vertices;
         mean_vectors = (unit_out_vectors-unit_in_vectors)/2;
         length_mean_vectors = sum(mean_vectors.^2,2).^0.5;
         unit_direction_of_cut = mean_vectors./length_mean_vectors;
         if flag_do_plot
+            % plots interior vertex normal
             vertex_normal_plot = quiver(vertices(1:end-1,1),vertices(1:end-1,2),unit_direction_of_cut(:,1),unit_direction_of_cut(:,2),'g');
         end
+        % interior vertex normals
         vertex_normal_vectors = [unit_direction_of_cut(:,1),unit_direction_of_cut(:,2)];
-        % vertex_normal_vectors = circshift(vertex_normal_vectors,1)
+        % vectors in directions of polytope sides
         side_vectors = zeros(length(vertices)-1,2);
+        % this will store the direction from the vertex normal to the side
         theta_normal_to_side = zeros(length(vertices)-1,1);
+        % this will store the angle between the vertex normal and travel direction
         angle_vertex_normal_to_travel_direction = NaN(length(vertices)-1,1);
+        % a boolean determining if vertex opens away from travel direction
         vertex_is_away = zeros(1, length(vertices)-1);
+        % a boolean determining if this vertex "blocks" the direction of travel
         travel_direction_is_within_polytope = zeros(1, length(vertices)-1);
+        % travel direction is assumed to be left ot right, 0 degrees from horizontal
         travel_direction = [1,0];
+        % these will store normals, vertices, and angles that block travel and are valid decision points
         away_normals = NaN(length(vertices)-1,2);
         away_vertices = NaN(length(vertices)-1,2);
         away_angles = NaN(length(vertices)-1,1);
+        % this will store side lengths associated with the chosen deflection direction (the small angle)
         chosen_side_lengths = NaN(length(vertices)-1,1);
         away_angle_vertex_normal_to_travel_direction = NaN(length(vertices)-1,1);
         % make distances array circular by putting the first entry at the end to support end+1 circular indexing
         shrinker.distances = [shrinker.distances;shrinker.distances(1)];
-        % end initialize path for iterative solution
+
         % begin looping through vertices
         for i=1:length(vertices)-1
             angle_vertex_normal_to_travel_direction(i) = INTERNAL_angle_between_vectors(vertex_normal_vectors(i,:),travel_direction);
             vertex_is_away(i) = dot(vertex_normal_vectors(i,:),travel_direction);
             travel_direction_is_within_polytope(i) = angle_vertex_normal_to_travel_direction(i)<=(pi-angles(i))/2;
-            % begin looping through away vertices
+            % begin looping through away vertices that block travel direction
             if vertex_is_away(i)>0 && travel_direction_is_within_polytope(i)
-                % pick a point at x=0 y=/= 0 to start iterative solution
+                % pick a point at x=0, y~=0 to start iterative solution
                 if vertices(i,1) == 0 && vertices(i,2) ~= 0 && start_not_found
                     start_location = vertices(i,:);
                     path = [path; start_location];
@@ -160,15 +168,19 @@ flag_do_debug = 0;     % Set equal to 1 for debugging
                 % end pick a point at x=0 y=/= 0 to start iterative solution
                 away_vertices(i,:) = vertices(i,:);
                 away_normals(i,:) = vertex_normal_vectors(i,:);
+                % angles need to be subtracted from 180 deg to get interior angles
                 away_angles(i) = pi-angles(i);
                 away_angle_vertex_normal_to_travel_direction(i) =  angle_vertex_normal_to_travel_direction(i);
-            % for the chosen divergence angle, find the length of the side we turned towards
+                % for the chosen divergence angle, find the length of the side we turned towards
                 if INTERNAL_is_left_turn_smaller(vertex_normal_vectors(i,:),travel_direction)
-                     side_length = shrinker.distances(i+1); % the vertex we're at has the side length to its left at the next index
+                    % the vertex we're at has the side length to its left at the next index
+                    side_length = shrinker.distances(i+1);
                  else
-                     side_length = shrinker.distances(i); % the vertex we're at has the side length associated with it on the right
+                    % the vertex we're at has the side length associated with it on the right
+                    side_length = shrinker.distances(i);
                 end
                 chosen_side_lengths(i) = side_length;
+
                 % log chosen_side_length and small angle separately for iterative solution
                 if vertices(i,:) == path(end,:)
                     iterative_chosen_side_lengths = [iterative_chosen_side_lengths, side_length];
@@ -191,52 +203,56 @@ flag_do_debug = 0;     % Set equal to 1 for debugging
             side_vectors(i,2) = vertices(i+1,2)-vertices(i,2);
             theta_normal_to_side(i) = INTERNAL_angle_between_vectors(side_vectors(i,:),vertex_normal_vectors(i,:));
             if flag_do_plot
+                % plot the side vectors on top of the original polytope to check for discrepancies
                 side_vec_plot = quiver(vertices(i,1),vertices(i,2),vertices(i+1,1)-vertices(i,1),vertices(i+1,2)-vertices(i,2),'-b');
                 travel_vec_plot = quiver(vertices(i,1),vertices(i,2),travel_direction(1)*0.05,travel_direction(2)*0.05,'-m');
             end
         % end looping through vertices
         end
-        % remove data for angles pointing towards travel direction
+        % remove data for angles pointing towards travel direction, these aren't validly blocking travel
         away_normals(any(isnan(away_angles),2),:)=[];
         away_angles(any(isnan(away_angles),2),:)=[];
         away_angle_vertex_normal_to_travel_direction(any(isnan(away_angle_vertex_normal_to_travel_direction),2),:)=[];
         chosen_side_lengths(any(isnan(chosen_side_lengths),2),:)=[];
-        % find large and small choice angles
+        % find large and small deflection choice angles
         small_choice_angles = away_angles./2-away_angle_vertex_normal_to_travel_direction;
         big_choice_angles = away_angles./2+away_angle_vertex_normal_to_travel_direction;
-        % log things for the entire field
+        % log data for the entire field
         field_away_normals = [field_away_normals;away_normals];
         field_away_angles = [field_away_angles;away_angles];
         field_away_angle_vertex_normal_to_travel_direction = [field_away_angle_vertex_normal_to_travel_direction;away_angle_vertex_normal_to_travel_direction];
         field_small_choice_angles = [field_small_choice_angles;small_choice_angles];
         field_big_choice_angles = [field_big_choice_angles;big_choice_angles];
         field_chosen_side_length = [field_chosen_side_length;chosen_side_lengths];
+
         % change chosen angle to effective angle based on gap size
         field_away_angles_effective = field_away_angles - atan2(gap_size,field_chosen_side_length);
         field_small_choice_angles_effective = field_small_choice_angles - atan2(gap_size,field_chosen_side_length);
         % change chosen side length to effective angle side length based on gap size
         field_chosen_side_length_effective = (field_chosen_side_length.^2+gap_size.^2).^0.5;
+        if flag_do_plot
+            % code to plot travel directions on each relevant vertex with angle labels
+            % with labels
+            for i=1:length(away_angles)
+                away_travel_vec_plot = ...
+                quiver(away_vertices(i,1),away_vertices(i,2),travel_direction(1)*0.05,travel_direction(2)*0.05,'-m');
+            end
+            for i=1:length(vertices)-1
+            side_vectors(i,1) = vertices(i+1,1)-vertices(i,1);
+            side_vectors(i,2) = vertices(i+1,2)-vertices(i,2);
 
-        % TODO put all plotting code behind debug flag
-        % plot
-        % for i=1:length(away_angles)
-        %     away_travel_vec_plot = quiver(away_vertices(i,1),away_vertices(i,2),travel_direction(1)*0.05,travel_direction(2)*0.05,'-m');
-        % end
-        % for i=1:length(vertices)-1
-        % side_vectors(i,1) = vertices(i+1,1)-vertices(i,1);
-        % side_vectors(i,2) = vertices(i+1,2)-vertices(i,2);
-
-        % theta = arccos((x dot y)/(mag x * mag y))
-        % theta_normal_to_side(i) = INTERNAL_angle_between_vectors(side_vectors(i,:),vertex_normal_vectors(i,:));
-        % label large and small sizes
-%         size = max(max(vertices)) - min(min(vertices));
-%         nudge = size*0.01;
-%         % Label the vertices
-%         for ith_angle = 1:length(angles)
-%             ith_vertex = ith_angle;
-%             text(vertices(ith_vertex,1)+nudge,vertices(ith_vertex,2),...
-%                 sprintf('%.0f deg',180-angles(ith_angle,1)*180/pi));
-%         end
+            theta_normal_to_side(i) = INTERNAL_angle_between_vectors(side_vectors(i,:),vertex_normal_vectors(i,:));
+            % label large and small sizes
+               size = max(max(vertices)) - min(min(vertices));
+               nudge = size*0.01;
+               % Label the vertices
+               for ith_angle = 1:length(angles)
+                   ith_vertex = ith_angle;
+                   text(vertices(ith_vertex,1)+nudge,vertices(ith_vertex,2),...
+                       sprintf('%.0f deg',180-angles(ith_angle,1)*180/pi));
+               end
+            end
+        end
     end
     % begin results generation
     r_lc_max = 1/cos(mean(field_away_angles)/2);
@@ -355,7 +371,7 @@ function ang = INTERNAL_angle_between_vectors(a,b)
     % vector that is rotated 182 deg CCW will return a value of 178 CW
     % this doesn't matter as we expect all vertex normal vectors we care
     % about to be in the direction of the path
-    % ang = atan2(norm(cross([a,0],[b,0])), dot(a,b));
+    % theta = arccos((x dot y)/(mag x * mag y))
     ang = acos(dot(a,b)/(norm(a)*norm(b)));
     if a(2)<0
         ang = ang;
