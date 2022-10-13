19d9c9f3b50df0fffb0d0245ec9e52a973d96add
sjharnett <sjh6473@psu.edu>
2022-10-13 11:46:44 -0400

    Fix bug where interpolation happens in delta_x not delta_length

--- Functions/fcn_MapGen_increasePolytopeVertexCount.m
+++ Functions/fcn_MapGen_increasePolytopeVertexCount.m
@@ -1,6 +1,6 @@
 function interpolated_polytopes = fcn_MapGen_increasePolytopeVertexCount(polytopes,resolution)
     resolution = 0.005; % TODO something better than a hard coded resolution could be used here
-    flag_do_plot = 0;
+    flag_do_plot = 1;
     for i = 1:length(shrunk_polytopes)
         new_verts = [];
         if flag_do_plot
@@ -12,13 +12,18 @@ function interpolated_polytopes = fcn_MapGen_increasePolytopeVertexCount(polytop
         for j = 1:(size(shrunk_polytopes(i).vertices,1)-1)
             % if this side goes backwards (from larger x to smaller x) we
             % have to flip the interpolated points
+            delta_x = shrunk_polytopes(i).vertices(j,1) - shrunk_polytopes(i).vertices(j+1,1);
+            delta_y = shrunk_polytopes(i).vertices(j,2) - shrunk_polytopes(i).vertices(j+1,2);
+            side_length = sqrt(delta_x^2+delta_y^2);
+            num_verts_needed_this_side = side_length/resolution;
+            resolution_in_x = abs(delta_x)/num_verts_needed_this_side;
             if shrunk_polytopes(i).vertices(j,1) < shrunk_polytopes(i).vertices(j+1,1)
-                xq = shrunk_polytopes(i).vertices(j,1):resolution:shrunk_polytopes(i).vertices(j+1,1);
+                xq = shrunk_polytopes(i).vertices(j,1):resolution_in_x:shrunk_polytopes(i).vertices(j+1,1);
                 vq = interp1([shrunk_polytopes(i).vertices(j,1),shrunk_polytopes(i).vertices(j+1,1)],...
                                 [shrunk_polytopes(i).vertices(j,2),shrunk_polytopes(i).vertices(j+1,2)],...
                                 xq);
             else
-                xq = shrunk_polytopes(i).vertices(j+1,1):resolution:shrunk_polytopes(i).vertices(j,1);
+                xq = shrunk_polytopes(i).vertices(j+1,1):resolution_in_x:shrunk_polytopes(i).vertices(j,1);
                 vq = interp1([shrunk_polytopes(i).vertices(j+1,1),shrunk_polytopes(i).vertices(j,1)],...
                                 [shrunk_polytopes(i).vertices(j+1,2),shrunk_polytopes(i).vertices(j,2)],...
                                 xq);
