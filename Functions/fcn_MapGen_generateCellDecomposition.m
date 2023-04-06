c32db29e3d71c774c6a9d0a040cde4246437a699
sjharnett <sjh6473@psu.edu>
2023-04-06 15:24:28 -0400

    starting cell decomposition code

---
+++ Functions/fcn_MapGen_generateCellDecomposition.m
@@ -0,0 +1,12 @@
+cells = fcn_MapGen_generateCellDecomposition(polytopes)
+    % sweep vertical bar from left to right
+    % generate all points
+    % find left most point
+    % does this polytope have points both left and right of this point?
+    % if not, draw line all the way up and down
+    % if so, are the other points above or below this one?
+    % if above, draw line below
+    % if below, draw line above
+
+    % if we have a list of sides, we can draw until the nearest side
+end
