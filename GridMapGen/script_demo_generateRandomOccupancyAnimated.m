7eb200ee351a3cbe3dd6cf9300d7bb6025a817ea
IVSG <67085752+ivsg-psu@users.noreply.github.com>
2025-07-21 10:34:23 -0400

    FEAT: updated generateRandomOccupancyAnimated to allow larger motion, smoothed the color

--- GridMapGen/script_demo_generateRandomOccupancyAnimated.m
+++ GridMapGen/script_demo_generateRandomOccupancyAnimated.m
@@ -35,42 +35,64 @@ optimizedThreshold = [];
     'flagSkipThresholdOptimization',(0),...% [1x1] scalar
     'figNum',(-1));
 
+colorMin = min(randomMatrixDilated,[],"all");
+colorMax = max(randomMatrixDilated,[],"all");
+
 fig_num = 1111;
 figure(fig_num); clf;
 numColors = 256;
 cmap = turbo(numColors);
+colormap(cmap);
 
 h_fig = figure(fig_num);
 set(h_fig,'Name','animatedRandom','NumberTitle','off'); %, 'Position',[684 85 592 317]);
 
 for ith_step = 1:50 %:Nsteps
     
-    % Resample top Nrand values
-    % Increasing this number causes objects to "disappear" more as they
-    % progress in time
-    Nrand = 20;
-    seedVector = reshape(seedMap,[],1);
-    [~,sortedRandInd] = sort(seedVector,'descend');
-    seedVector(sortedRandInd(1:Nrand),1) = rand(Nrand,1);
-    seedMap = reshape(seedVector,nRows,mColumns);
+    % % Resample top Nrand values
+    % % Increasing this number causes objects to "disappear" more as they
+    % % progress in time
+    % Nrand = 20;
+    % seedVector = reshape(seedMap,[],1);
+    % [~,sortedRandInd] = sort(seedVector,'descend');
+    % seedVector(sortedRandInd(1:Nrand),1) = rand(Nrand,1);
+    % seedMap = reshape(seedVector,nRows,mColumns);
 
+    %%%%%%%%%%%
+    % Change the map slightly to "evolve" the seeds
     % Resample Nrand values
-    Nrand = 0;
+    Nrand = 20;
     randomThreshold = Nrand/Nseeds;
     randomChange = rand(nRows,mColumns);
     indicesToChange = find(randomChange<randomThreshold);
     seedMap(indicesToChange) = rand(length(indicesToChange),1);
 
+    %%%%%%%%%%%
     % Randomly walk sideways
-    percentageSideways = 0.5; % A value between 0 and 1
-    randomChange = rand(nRows,mColumns);
-    indicesChange = find(randomChange<percentageSideways);
-    % Do not walk last column
-    indicesChange = indicesChange(indicesChange<(nRows*(mColumns-1)));
-    seedMap(indicesChange+nRows) = seedMap(indicesChange);
-    firstColumnChanged = find(indicesChange<=nRows);
-    seedMap(firstColumnChanged) = rand(length(firstColumnChanged),1);  
+    movementSideways = 0.6; %.5; %2.3;
+    percentageSideways = mod(movementSideways,1); % A value between 0 and 1
+    columnsSideways = floor(movementSideways);
+    
+    % Move the percentage
+    if percentageSideways>0
+        % Do not walk last columns, and refill first columns
+        randomChange = rand(nRows,mColumns);
+        indicesChange = find(randomChange<percentageSideways);
+        indicesChange = indicesChange(indicesChange<(nRows*(mColumns-1)));
+        seedMap(indicesChange+nRows) = seedMap(indicesChange);
+        firstColumnChanged = find(indicesChange<=nRows);
+        seedMap(firstColumnChanged) = rand(length(firstColumnChanged),1);
+    end
 
+    % Move the columns
+    if columnsSideways>0
+        randomChange = rand(nRows,columnsSideways);
+        seedMap = [randomChange seedMap(:,1:(mColumns-columnsSideways))];
+    end
+
+
+    %%%%%%%%%%%
+    % Update the map based on changed seed
     % Call the function once to initialize settings for upcoming calls
     [~, randomMatrixDilated, updatedThreshold, leftDilationMultiplier, rightDilationMultiplier] = ...
         fcn_GridMapGen_generateRandomOccupancyMap(...
@@ -84,17 +106,22 @@ for ith_step = 1:50 %:Nsteps
     % 'occupancyRatio',(occupancyRatio),... % [1x1] value between 0 and 1
     forcedThreshold = 0.9*forcedThreshold + 0.1*updatedThreshold;
 
-    %% See:
-    % https://www.mathworks.com/matlabcentral/answers/194554-how-can-i-use-and-display-two-different-colormaps-on-the-same-figure
-    
-    colorizedImage = floor(rescale(randomMatrixDilated,1,numColors));
+    %%%%
+    % Rescale the colors to integer values
+
+    % Filter the color minimum and max values
+    keep = 0.8;
+    colorMin = (keep)*colorMin + (1-keep)*min(randomMatrixDilated,[],"all");
+    colorMax = (keep)*colorMax + (1-keep)*max(randomMatrixDilated,[],"all");
+
+    rescaledToColorIntegers = fcn_INTERNAL_rescaleToColors(randomMatrixDilated, colorMin, colorMax, numColors);
     clf;
-    image(colorizedImage);
-    colormap(gca, cmap);
+    image(rescaledToColorIntegers);
     hold on;
-    [M,c] = contour(randomMatrixDilated,50,'w-','Linewidth',0.2);
+    [M,c] = contour(randomMatrixDilated,50,'k-','Linewidth',0.2);
 
     if 1==0
+        % Trying to use a different color map for the contours
         %% See:
         % https://www.mathworks.com/matlabcentral/answers/194554-how-can-i-use-and-display-two-different-colormaps-on-the-same-figure
         colorizedImage = floor(rescale(randomMatrixDilated,1,numColors));
@@ -121,3 +148,13 @@ for ith_step = 1:50 %:Nsteps
     end
     pause(0.01);
 end
+
+%% fcn_INTERNAL_rescaleToColors
+function rescaledToColorIntegers = fcn_INTERNAL_rescaleToColors(randomMatrixDilated, colorMin, colorMax, numColors)
+
+colorInterval = (colorMax-colorMin)/(numColors-1); % The interval represents the "jump" between different colors
+offsetRemovedMatrix = randomMatrixDilated - colorMin; % Remove the offset
+countMatrix = floor(offsetRemovedMatrix./colorInterval)+1;
+rescaledToColorIntegers = min(max(countMatrix,1),numColors);
+
+end % Ends fcn_INTERNAL_rescaleToColors
